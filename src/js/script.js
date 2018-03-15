75e5caa50108b197965e8b08276983fcd1d0dd8a
viktorasri <viktoras.rimkus@gmail.com>
2018-03-15 11:48:41 +0200

    script.js file updated with comments

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+
+
+//Form data template. Filled data will be saved to this object.
 var loanForm = {
 	"step1": {
 		"credit_type": "",
@@ -16,7 +19,7 @@ var loanForm = {
 	"step5": {
 		"info": ""
 	}	
-}
+};
 
 
 
@@ -24,54 +27,62 @@ var ctaBtn = document.getElementById('cta-loan');
 var intro = document.querySelector('.small-loan__starter');
 var form = document.querySelector('.small-loan__form')
 
+//Intro/starter call to action button.
 ctaBtn.addEventListener('click', ()=> {
-	intro.style.display = "none";
-	showQuestion(pageNo);
-	form.style.display = "block";
+	intro.style.display = "none";   //hide intro/starter block
+	showQuestion(pageNo);           //call to questions paging function
+	form.style.display = "block";   //form visibility from none changed to block
 });
 
 
+//select all questions in form
 var formSteps = document.querySelectorAll('.small-loan__form__step');
+
+//default paging number
 var pageNo = 0;
 
+//Paging function displays 1 question per page
 function showQuestion(pageNo) {
-	formSteps.forEach(step=> step.style.display = "none" );
-	formSteps[pageNo].style.display = "flex"
+	formSteps.forEach(step=> step.style.display = "none" );  //firstly hide all questions nodes
+	formSteps[pageNo].style.display = "flex"                 //set display property flex to node referred by pageNo.
 }
 
 
-
-
-
+//Returns select value from select input
 function getSelectValue(select) {
 	return select[select.selectedIndex].value;
 }
 
+//Used for must to fill radio buttons to checks if user selected any option.
 function validateRadio(radio) {
 	var validation = false;
 	radio.forEach(el=> {
 		if (el.checked) {
-			validation = true;
+			validation = true;      //in case user selected any option, validation set tu true.
 			return
 		}
 	});
-	!validation? showAlert("Please select radio button value") : null;
-	return validation
+	!validation? showAlert("Please select radio button value") : null;  //Message to user in case validation is false
+	return validation   //returns validation value
 }
 
 
-var alerAnchor = document.querySelector('.progress-bar')
+var alerAnchor = document.querySelector('.progress-bar');
+
+//Display alert message text in web form.
 function showAlert(msg) {
-	var content = `<div class="errormsg">${msg}</div>`
-	alerAnchor.insertAdjacentHTML('afterend',content)
+	var content = `<div class="errormsg">${msg}</div>`;
+	alerAnchor.insertAdjacentHTML('afterend',content);  //insert alert message after anchor DOM element
 }
 
+//Removes alert message from web form
 function removeAler() {
-	var alert = document.querySelector('.errormsg')
+	var alert = document.querySelector('.errormsg');
 	if (!alert) return;
 	return alert.parentNode.removeChild(alert);
 }
 
+//returns radio inputs value which is selected by user
 function getRadioValue(radio) {
 	var value = '';
 	radio.forEach(el=> {
@@ -80,10 +91,10 @@ function getRadioValue(radio) {
 			return
 		}
 	});
-	
 	return value
 }
 
+//checks if text inputs if filled (at least one character)
 function required(item) {
 	if (item.length === 0) {
 		showAlert("The mandatory information is missing or incorrect!");
@@ -92,26 +103,28 @@ function required(item) {
 	return true
 }
 
+//validates text input value. Text input must be letters and space only.
 function validateText(input) {
 	if (!input.match(/^[A-Za-z\s]*$/)) {
-		showAlert("The mandatory information must be letters only!");
+		showAlert("The mandatory information must be letters only!");   //Message to user in case validation is false
 		return false;
 	}
 	return true
 }
 
+//validates text input value. Text input must be letters and numbers only.
 function validateTextNumbers(input) {
 	if (!input.match(/^[0-9a-zA-Z]+$/)) {
-		showAlert("The mandatory information must be letters & numbers only!");
+		showAlert("The mandatory information must be letters & numbers only!"); //Message to user in case validation is false
 		return false;
 	}
 	return true
 }
 
+//validates number input value. Number must be >0 and max length 12.
 function validateNumber(input) {
-	
 	if ((parseFloat(input) < 0) || (input.length > 12)) {
-		showAlert("Please enter correct amount (from 0 to 12 digits)");
+		showAlert("Please enter correct amount (from 0 to 12 digits)"); //Message to user in case validation is false
 		return false;
 	}
 	return true
@@ -119,82 +132,81 @@ function validateNumber(input) {
 
 
 
-
-
+//Back button in the questions form
 var buttonBack = document.querySelectorAll('.js-back');
-buttonBack[0].style.visibility='hidden';
+buttonBack[0].style.visibility='hidden';        //button is hidden then in first question page
 buttonBack.forEach(btn => {
 	btn.addEventListener('click',()=> {
-		removeAler();
-		progressBarOnBack(pageNo);
+		removeAler();                           //removes all alerts from previous page
+		progressBarOnBack(pageNo);              //Update progress bar
 		
 		if (pageNo>0) {
-			pageNo--;
+			pageNo--;                           //decrease page number value by 1
 		}
 		
-		showQuestion(pageNo);
+		showQuestion(pageNo);                   //call paging function to display updated pageNo
 	})
 });
 
-
+//first question forward button validation and controls
 var applicationForwardBtn = document.getElementById('application-forward');
 applicationForwardBtn.addEventListener('click', ()=>{
-	removeAler();
+	removeAler();                                                   //removes all alerts
 	var credit_type = document.getElementById('credit_type');
-	if (!required(getSelectValue(credit_type))) return;
+	if (!required(getSelectValue(credit_type))) return;             //validates input. If value not validated returns
 	loanForm.step1.credit_type = getSelectValue(credit_type);
 	
 	var loan_amount = document.getElementById('loan_amount').value;
-	if (!required(loan_amount) || !validateNumber(loan_amount)) return;
+	if (!required(loan_amount) || !validateNumber(loan_amount)) return; //validates input. If value not validated returns
 	loanForm.step1.loan_amount = loan_amount;
 	
 	var loan_term = document.getElementById('loan_term');
-	if (!required(getSelectValue(loan_term))) return;
+	if (!required(getSelectValue(loan_term))) return;               //validates input. If value not validated returns
 	loanForm.step1.loan_term = getSelectValue(loan_term);
 	
-	pageNo++;
-	showQuestion(pageNo);
-	progressBar(pageNo);
+	pageNo++;                   //Increase page number value by 1
+	showQuestion(pageNo);       //call paging function to display updated pageNo
+	progressBar(pageNo);        //Update progress bar
 });
 
 
-
+//second question forward button validation and controls
 var gender = document.getElementsByName('gender');
 var contactsForwardBtn = document.getElementById('contacts-forward');
 contactsForwardBtn.addEventListener('click', ()=>{
-	removeAler();
+	removeAler();                                                   //removes all alerts
 	var fname = document.getElementById('fname').value;
-	if (!required(fname) || !validateText(fname) ) return;
+	if (!required(fname) || !validateText(fname) ) return;          //validates input. If value not validated returns
 	loanForm.step2.fname = fname;
 	
 	var lname = document.getElementById('lname').value;
-	if (!required(lname) || !validateText(lname) ) return;
+	if (!required(lname) || !validateText(lname) ) return;          //validates input. If value not validated returns
 	loanForm.step2.lname = lname;
 	
 	var gender = document.getElementsByName('gender');
-	if (!validateRadio(gender)) return;
+	if (!validateRadio(gender)) return;                             //validates input. If value not validated returns
 	loanForm.step2.gender = getRadioValue(gender);
 	
 	var docNo = document.getElementById('docNo').value;
-	if (!required(docNo) || !validateTextNumbers(docNo) ) return;
+	if (!required(docNo) || !validateTextNumbers(docNo) ) return;   //validates input. If value not validated returns
 	loanForm.step2.docNo = docNo;
 	
-	pageNo++;
-	showQuestion(pageNo);
-	progressBar(pageNo);
+	pageNo++;                   //Increase page number value by 1
+	showQuestion(pageNo);       //call paging function to display updated pageNo
+	progressBar(pageNo);        //Update progress bar
 });
 
-
+//third question forward button validation and controls. This question is not mandatory
 var obligationForwardBtn = document.getElementById('obligation-forward');
 obligationForwardBtn.addEventListener('click', ()=>{
-	removeAler();
+	removeAler();                                                       //removes all alerts
 	var oblType = document.getElementById('obligation-type');
 	var oblFinancer = document.getElementById('obligation-financer');
 	var oblTerm = document.getElementById('obligation-term').value;
 	var oblAmount = document.getElementById('obligation-amount').value;
 	var oblPayment = document.getElementById('obligation-monthly-payement').value;
 	
-	
+	//if user fill in any input, all other become mandatory to fill as well
 	if ( (getSelectValue(oblType) != "") || (getSelectValue(oblFinancer) != "") ||
 		(oblTerm != "") || (oblAmount != "") || (oblPayment != "")) {
 		if (!required(getSelectValue(oblType)))return;
@@ -203,35 +215,38 @@ obligationForwardBtn.addEventListener('click', ()=>{
 		if (!required(oblAmount) || !validateNumber(oblAmount))return;
 		if (!required(oblPayment) || !validateNumber(oblPayment))return;
 		
+		//object property (new object) in form template is created
 		if (!loanForm["step3"]){
 			loanForm["step3"] = {};
 		}
 		
+		//object properties to new object assigned
 		loanForm.step3.oblType = getSelectValue(oblType);
 		loanForm.step3.oblFinancer = getSelectValue(oblFinancer);
 		loanForm.step3.oblTerm = oblTerm;
 		loanForm.step3.oblAmount = oblAmount;
 		loanForm.step3.oblPayment = oblPayment;
 		
-	} else if (loanForm["step3"]) delete loanForm.step3;
+	} else if (loanForm["step3"]) delete loanForm.step3;            //step3 object property removed if user do not finish to fill in data
 	
-	pageNo++;
-	showQuestion(pageNo);
-	progressBar(pageNo);
+	pageNo++;                   //Increase page number value by 1
+	showQuestion(pageNo);       //call paging function to display updated pageNo
+	progressBar(pageNo);        //Update progress bar
 });
 
-
+//Fourth question forward button validation and controls.
 var incomeForwardBtn = document.getElementById('income-forward');
 incomeForwardBtn.addEventListener('click', ()=>{
-	removeAler();
+	removeAler();                                                   //removes all alerts
 	var salary = document.getElementById('salary').value;
-	if (!required(salary) || !validateNumber(salary)) return;
+	if (!required(salary) || !validateNumber(salary)) return;       //validates input. If value not validated returns
 	loanForm.step4.salary = salary;
 	
+	//question is not mandatory
 	var incomeOther = document.getElementById('income-other').value;
-	if ((incomeOther !== "") && validateNumber(incomeOther)) {
-		loanForm.step4.incomeOther = incomeOther;
-	} else if (loanForm.step4["incomeOther"]) delete loanForm.step4.incomeOther;
+	if ((incomeOther !== "") && validateNumber(incomeOther)) {      //checks if any data entered and validates input.
+		loanForm.step4.incomeOther = incomeOther;                   //creates new property in form data object, step4
+	} else if (loanForm.step4["incomeOther"]) delete loanForm.step4.incomeOther;  //if validation fails property from form data object removed
 	
 	var employer = document.getElementById('selfEmployer');
 	if (employer.checked) {
@@ -240,37 +255,38 @@ incomeForwardBtn.addEventListener('click', ()=>{
 		loanForm.step4.employer = "worker"
 	};
 	
-	pageNo++;
-	showQuestion(pageNo);
-	progressBar(pageNo);
+	pageNo++;                   //Increase page number value by 1
+	showQuestion(pageNo);       //call paging function to display updated pageNo
+	progressBar(pageNo);        //Update progress bar
 });
 
-
+//Last question forward/confirm button validation and controls.
 var confirmBtn = document.getElementById('confirm');
 confirmBtn.addEventListener('click', ()=>{
-	removeAler();
+	removeAler();                                                   //removes all alerts
 	var info = document.getElementById('additional-info').value;
+	//checks if any data entered
 	if (info !== "") {
 		loanForm.step5.info = info;
 	} else {
 		loanForm.step5.info = "No additional information";
 	}
 	
-	console.log(loanForm)
-	form.style.display = "none";
-	printOut();
-	document.querySelector('.small-loan__results').style.display = 'block'
+	
+	form.style.display = "none";            //hide form block
+	printOut();                             //call printout function to print entered data
+	document.querySelector('.small-loan__results').style.display = 'block'; //results block element with printed data visibility
 	
 });
 
-
+//tracking if incomes inputs values is updated
 var allIncomes = document.querySelectorAll('.js-allIncomes');
 allIncomes.forEach(income => {
 	income.addEventListener('change', sumarize);
 	income.addEventListener('keyup',sumarize);
 });
 
-
+//Calculates total incomes and show value in form
 function sumarize() {
 	var items = document.querySelectorAll('.js-allIncomes');
 	var showTotIncome = document.getElementById('total-income');
@@ -281,21 +297,26 @@ function sumarize() {
 	showTotIncome.textContent = `${sum} EUR`;
 }
 
-
+//reordering/sorting object properties
 function sortObject(o) {
 	return Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {});
 }
 
 
-var printContainer = document.querySelector('.print-out')
+var printContainer = document.querySelector('.print-out');
+//print filled form data to selected block
 function printOut() {
 	var html = '';
 	
-	for (var step in sortObject(loanForm)){
-		html += `<h3 class="heading-tertiary">${printValues[step]}</h3>`;
+	for (var step in sortObject(loanForm)){                                 //go through all properties in loanForm. Sort steps in case user filled not mandatory info
+		html += `<h3 class="heading-tertiary">${printValues[step]}</h3>`;   //Printout question heading. Print value taken from printValues object
 		
+		//construct printout content for every step/question
 		var content = '';
-		for (var stepkey in loanForm[step]) {
+		for (var stepkey in loanForm[step]) {                               //go through all properties in step/question object
+			
+			//Questions print values is taken from printValues object
+			//Answers: firstly check for values in printValues object (questions with predefined answers). Original text or number printed for text, number, textarea inputs.
 			content += `
 					<li class="small-loan__results__items">
 						<span class="small-loan__results__key">${printValues[stepkey]}</span>
@@ -304,6 +325,7 @@ function printOut() {
 			`;
 		};
 		
+		//adds every step content
 		html += `<ul class="small-loan__results__list">${content}</ul>`;
 	}
 	printContainer.innerHTML = html;
@@ -312,19 +334,21 @@ function printOut() {
 var filler = document.querySelector('.progress-bar__h-line--filler')
 var progresBarBulbs = document.querySelectorAll('.progress-bar__item')
 
+//progress bar functionality on forward
 function progressBar(el) {
-	progresBarBulbs[el].style.backgroundColor = "#ee7023";
-	progresBarBulbs[el-1].innerHTML = '<i class="fa fa-check"></i>'
-	filler.style.width = `${el*25}%`
+	progresBarBulbs[el].style.backgroundColor = "#ee7023";              //update color to current step in progress bar
+	progresBarBulbs[el-1].innerHTML = '<i class="fa fa-check"></i>'     //change icon on previous step
+	filler.style.width = `${el*25}%`                                    //update progress line/filler
 }
 
+//progress bar functionality on back
 function progressBarOnBack(el) {
-	progresBarBulbs[el].style.backgroundColor = "#fdc129";
-	progresBarBulbs[el-1].innerHTML = `${el}`;
-	filler.style.width = `${el*25}%`
+	progresBarBulbs[el].style.backgroundColor = "#fdc129";              //update color to current step in progress bar
+	progresBarBulbs[el-1].innerHTML = `${el}`;                          //changes progress bulb content to step number
+	filler.style.width = `${el*25}%`                                    //update progress line/filler
 }
 
-
+//object with printout values
 var printValues = {
 	"step1":"Loan details",
 	"step2":"Contact information",
